85e44f2538567022358bb951901b3242f9f57ef9
Jesus Valdivia <68979059+jesusvaldivia@users.noreply.github.com>
2022-11-17 20:24:10 -0500

    remove mongo env

--- mod/html5/entrypoint.sh
+++ mod/html5/entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 cd /app
-export MONGO_OPLOG_URL=mongodb://10.7.7.6/local
-export MONGO_URL=mongodb://10.7.7.6/meteor
-export ROOT_URL=http://127.0.0.1/html5client
+
 export NODE_ENV=production
 export SERVER_WEBSOCKET_COMPRESSION='{"level":5, "maxWindowBits":13, "memLevel":7, "requestMaxWindowBits":13}'
 export BIND_IP=0.0.0.0
